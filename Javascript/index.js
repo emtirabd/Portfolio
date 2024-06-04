4952df0fb88e5cf5c4161f6bcfe5e673d808e406
emtirabd <emtir1902@gmail.com>
2024-06-04 20:19:53 +0200

    first commit (navbar)

---
+++ Javascript/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link a");
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", function() {
+        // Remove 'active' class from all links
+        navLinks.forEach(link => link.classList.remove("active"));
+        
+        // Add 'active' class to the clicked link
+        this.classList.add("active");
+      });
+    });
+  });
+  
